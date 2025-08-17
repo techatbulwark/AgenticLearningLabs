782525fd898edb1bedce5f19f3a54028253b9d59
jyoon9401 <jyoon9401@gmail.com>
2025-08-17 16:17:50 -0400

    env config

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,6 @@ print(f'supabase env {settings.supabase_url, settings.supabase_secret_key}')
 SUPABASE_URL = settings.supabase_url
 SUPABASE_SECRET_KEY = settings.supabase_secret_key
 
-create_client()
 supabase_client: Client = create_client(SUPABASE_URL, SUPABASE_SECRET_KEY)
 
 def get_client():
