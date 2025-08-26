3a894298953c5f203c1cf078e8b9f44a3cf72ff0
jyoon9401 <jyoon9401@gmail.com>
2025-08-26 15:35:27 -0400

    resend email rollback

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,10 @@
 from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from typing import Optional, List
+import json
 
 class Settings(BaseSettings):
+    
     # database
     supabase_url: Optional[str] = None
     supabase_secret_key: Optional[str] = None
@@ -12,12 +14,14 @@ class Settings(BaseSettings):
     smtp_port: Optional[int] = 587
     smtp_user: Optional[str] = None
     smtp_password: Optional[str] = None
-    recipient_emails: Optional[str] = None
 
     # resend
     resend_key: Optional[str] = None
     resend_sender: Optional[str] = None
-    resend_recipients: List[str] = None
+    resend_recipients: Optional[str] = None
+    @property
+    def resend_recipients_list(self) -> List[str]:
+        return json.loads(self.resend_recipients)
     
     # url
     frontend_url: Optional[str] = None
