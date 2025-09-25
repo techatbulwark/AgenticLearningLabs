35d60d89dd6f86785794840e957ef26d08c21e54
Bradley Ross <bar181@yahoo.com>
2025-09-25 19:40:07 +0000

    updated calendar images

--- frontend/src/pages/DataAnalytics.jsx
+++ frontend/src/pages/DataAnalytics.jsx
@@ -6,7 +6,9 @@ import Footer from '../components/Footer';
 
 import course_header1 from '../assets/images/course_header1.jpg';
 import data from '../assets/images/data.png';
-import dataMobile from '../assets/images/data_mobile.png';
+// import dataMobile from '../assets/images/data_mobile.png';
+import dataMobile from '../assets/images/DADM.jpg';
+
 import HeaderLogo from '../components/HeaderLogo';
 import CourseUpdate from '../components/CourseUpdate.jsx';
 
